65f5a8996185fd61bdb65bdb9f157f7bf48b8fbd
Gabriel Almeida <gabrielsantos.almeida@hotmail.com>
2021-03-11 20:58:12 -0300

    finish challenger

--- src/index.js
+++ src/index.js
@@ -10,19 +10,66 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const findUser = users.find(user => user.username === username);
+
+  if (!findUser) {
+    return response.status(404).json({ error: 'User does not exist!' });
+  }
+
+  request.user = findUser;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ((!user.pro && user.todos.length < 10) || user.pro) {
+    return next();
+  } else {
+    return response.status(403).json({ error: 'User not availability!' });
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  let todo;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'User id invalid!' });
+  } else if (user) {
+    todo = user.todos.find(todo => todo.id === id);
+    if (!todo) {
+      return response.status(404).json({ error: 'Todo does not exist!' });
+    }
+  } else {
+    return response.status(404).json({ error: 'Todo does not exist!' });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const findUser = users.find(user => user.id === id);
+
+  if (!findUser) {
+    return response.status(404).json({ error: 'User does not exist!' });
+  }
+
+  request.user = findUser;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
